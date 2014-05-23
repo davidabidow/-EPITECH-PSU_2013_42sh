f5db86e1f2818c59a73e94e5fdf764eb0835e225
briard_g <briard_g@epitech.eu>
2014-05-23 22:35:51 +0200

    Makefile + main globbing

---
+++ globbing/main.c
@@ -0,0 +1,28 @@
+/*
+** main.c for main in /home/briard_g/rendu/PSU_2013_42sh/globbing
+** 
+** Made by Guillaume Briard
+** Login   <briard_g@epitech.net>
+** 
+** Started on  Fri May 23 18:11:43 2014 Guillaume Briard
+** Last update Fri May 23 22:34:54 2014 Guillaume Briard
+*/
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <glob.h>
+
+char		**check_glob(char *path)
+{
+  glob_t	*pglob;
+
+
+
+  return (pglob.gl_pathv);
+}
+
+int	main(int ac, char **av)
+{
+
+  return (EXIT_SUCCESS);
+}
