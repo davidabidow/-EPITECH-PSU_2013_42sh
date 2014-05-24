e1cb5c4600fbb8b38da3388c3d3ff88ff2bd666d
wallet_v <valentin.wallet@epitech.eu>
2014-05-24 05:01:05 +0200

    probleme tab regle

--- lib/epur_str.c
+++ lib/epur_str.c
@@ -5,42 +5,52 @@
 ** Login   <tran_0@epitech.net>
 ** 
 ** Started on  Tue May  6 15:53:46 2014 david tran
-** Last update Sat May 24 04:18:34 2014 david tran
+** Last update Sat May 24 04:58:19 2014 valentin wallet
 */
 
 #include "my.h"
 
-void	feed_epur(char *dest, char *str, int *i, int *j)
+void		feed_epur(char *dest, char *str, int *i, int *j)
 {
   dest[(*j)++] = ' ';
   dest[(*j)++] = str[(*i)++];
 }
 
-char	*epur_space(char *str)
+void            epur_tab(char *str)
 {
-  char	*dest;
-  int	i;
-  int	j;
-  char	flag;
+  int           i;
+
+  i = 0;
+  while (str[i] != '\0')
+    {
+      if (str[i] == '\t')
+        str[i] = ' ';
+      i++;
+    }
+}
+
+char		*epur_space(char *str)
+{
+  char          *dest;
+  int           i;
+  int           j;
 
   i = 0;
   j = 0;
-  flag = 0;
-  if (!(dest = my_xmalloc(sizeof(char) * (my_strlen(str) + 1))))
+  epur_tab(str);
+  if ((dest = malloc(sizeof(char) * (my_strlen(str) + 1))) == NULL)
     return (NULL);
-  while (str[i])
+  while (str[i] == ' ')
+    i++;
+  while (str[i] != '\0')
     {
-      if (str[i] && (str[i] == ' ' || str[i] == '\t'))
-	i++;
-      else if (i != 0 && (str[i - 1] == ' ' || str[i - 1] == '\t')
-	       && str[i] != ' ' && str[i] != '\t' && flag != 0)
-	feed_epur(dest, str, &i, &j);
-      else
-	{
-	  dest[j++] = str[i++];
-	  flag = 1;
-	}
+      while (str[i] != ' ' && str[i] != '\0')
+        dest[j++] = str[i++];
+      while (str[i] == ' ')
+        i++;
+      if (str[i] != '\0')
+        dest[j++] = ' ';
     }
-  dest[i] = 0;
+  dest[j] = '\0';
   return (dest);
 }
